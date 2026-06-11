ef551761993e7114a0b4c977cbcfa0293cd0e405
lowlly <yihoo2000@gmail.com>
2026-06-11 16:21:34 +0900

    Fix Xcode Cloud Flutter install: use git clone instead of brew

--- ios/ci_scripts/ci_pre_xcodebuild.sh
+++ ios/ci_scripts/ci_pre_xcodebuild.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 set -e
 
-echo "=== Installing Flutter ==="
-brew install flutter
+FLUTTER_HOME="$HOME/flutter"
+
+echo "=== Installing Flutter via git clone ==="
+if [ ! -d "$FLUTTER_HOME" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1 "$FLUTTER_HOME"
+fi
+
+export PATH="$PATH:$FLUTTER_HOME/bin"
+flutter --version
 
 echo "=== Flutter pub get ==="
 cd "$CI_PRIMARY_REPOSITORY_PATH"
@@ -12,4 +19,4 @@ echo "=== Pod install ==="
 cd "$CI_PRIMARY_REPOSITORY_PATH/ios"
 pod install --repo-update
 
-echo "=== Done ==="
+echo "=== Complete ==="
